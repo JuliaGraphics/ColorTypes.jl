9986cf1914c13c4a14ddfddbe420cac4f4020a8a
Simon Danisch <sdanisch@gmail.com>
2015-03-17 00:02:20 +0100

    added color types form Color.jl

--- src/types.jl
+++ src/types.jl
@@ -80,3 +80,153 @@ immutable HSI{T<:FloatingPoint} <: ColorValue{T}
 
     HSI(h::Real, s::Real, i::Real) = new(h, s, i)
 end
+
+
+
+
+
+# sRGB (standard Red-Green-Blue)
+immutable RGB{T<:Fractional} <: AbstractRGB{T}
+    r::T # Red [0,1]
+    g::T # Green [0,1]
+    b::T # Blue [0,1]
+end
+
+
+typemin{T}(::Type{RGB{T}}) = RGB{T}(zero(T), zero(T), zero(T))
+typemax{T}(::Type{RGB{T}}) = RGB{T}(one(T),  one(T),  one(T))
+
+# HSV (Hue-Saturation-Value)
+immutable HSV{T<:FloatingPoint} <: ColorValue{T}
+    h::T # Hue in [0,360]
+    s::T # Saturation in [0,1]
+    v::T # Value in [0,1]
+end
+HSV{T<:FloatingPoint}(h::T, s::T, v::T) = HSV{T}(h, s, v)
+HSV(h, s, v) = HSV{Float64}(h, s, v)
+HSV() = HSV(0.0, 0.0, 0.0)
+
+
+HSB(h, s, b) = HSV(h, s, b)
+
+
+# HSL (Hue-Lightness-Saturation)
+immutable HSL{T<:FloatingPoint} <: ColorValue{T}
+    h::T # Hue in [0,360]
+    s::T # Saturation in [0,1]
+    l::T # Lightness in [0,1]
+end
+
+
+HLS(h, l, s) = HSL(h, s, l)
+
+
+# XYZ (CIE 1931)
+immutable XYZ{T<:Fractional} <: ColorValue{T}
+    x::T
+    y::T
+    z::T
+end
+
+
+# CIE 1931 xyY (chromaticity + luminance) space
+immutable xyY{T<:FloatingPoint} <: ColorValue{T}
+    x::T
+    y::T
+    Y::T
+end
+xyY{T<:FloatingPoint}(x::T, y::T, Y::T) = xyY{T}(x, y, Y)
+xyY(x, y, Y) = xyY{Float64}(x, y, Y)
+xyY() = xyY(0.0,0.0,0.0)
+
+
+# Lab (CIELAB)
+immutable Lab{T<:FloatingPoint} <: ColorValue{T}
+    l::T # Luminance in approximately [0,100]
+    a::T # Red/Green
+    b::T # Blue/Yellow
+end
+
+
+typealias LAB Lab
+
+
+# LCHab (Luminance-Chroma-Hue, Polar-Lab)
+immutable LCHab{T<:FloatingPoint} <: ColorValue{T}
+    l::T # Luminance in [0,100]
+    c::T # Chroma
+    h::T # Hue in [0,360]
+end
+
+
+# Luv (CIELUV)
+immutable Luv{T<:FloatingPoint} <: ColorValue{T}
+    l::T # Luminance
+    u::T # Red/Green
+    v::T # Blue/Yellow
+end
+
+typealias LUV Luv
+
+
+# LCHuv (Luminance-Chroma-Hue, Polar-Luv)
+immutable LCHuv{T<:FloatingPoint} <: ColorValue{T}
+    l::T # Luminance
+    c::T # Chroma
+    h::T # Hue
+end
+
+
+# DIN99 (L99, a99, b99) - adaptation of CIELAB
+immutable DIN99{T<:FloatingPoint} <: ColorValue{T}
+    l::T # L99
+    a::T # a99
+    b::T # b99
+end
+
+
+
+# DIN99d (L99d, a99d, b99d) - Improvement on DIN99
+immutable DIN99d{T<:FloatingPoint} <: ColorValue{T}
+    l::T # L99d
+    a::T # a99d
+    b::T # b99d
+end
+
+
+
+# DIN99o (L99o, a99o, b99o) - adaptation of CIELAB
+immutable DIN99o{T<:FloatingPoint} <: ColorValue{T}
+    l::T # L99o
+    a::T # a99o
+    b::T # b99o
+end
+
+
+
+# LMS (Long Medium Short)
+immutable LMS{T<:FloatingPoint} <: ColorValue{T}
+    l::T # Long
+    m::T # Medium
+    s::T # Short
+end
+
+
+
+# 24 bit RGB and 32 bit ARGB (used by Cairo)
+# It would be nice to make this a subtype of AbstractRGB, but it doesn't have operations like c.r defined.
+immutable RGB24 <: ColorValue{Uint8}
+    color::Uint32
+end
+RGB24() = RGB24(0)
+RGB24(r::Uint8, g::Uint8, b::Uint8) = RGB24(uint32(r)<<16 | uint32(g)<<8 | uint32(b))
+RGB24(r::Ufixed8, g::Ufixed8, b::Ufixed8) = RGB24(reinterpret(r), reinterpret(g), reinterpret(b))
+
+immutable ARGB32 <: AbstractAlphaColorValue{RGB24, Uint8}
+    color::Uint32
+end
+ARGB32() = ARGB32(0)
+ARGB32(r::Uint8, g::Uint8, b::Uint8, alpha::Uint8) = ARGB32(uint32(alpha)<<24 | uint32(r)<<16 | uint32(g)<<8 | uint32(b))
+ARGB32(r::Ufixed8, g::Ufixed8, b::Ufixed8, alpha::Ufixed8) = ARGB32(reinterpret(r), reinterpret(g), reinterpret(b), reinterpret(alpha))
+
+AlphaColorValue(c::RGB24, alpha::Uint8 = 0xff) = AlphaColorValue{typeof(c),Uint8}(c, alpha)
