72a08ad94c0b18b8dc881a2c4157f49ffa8983a2
Tim Holy <tim.holy@gmail.com>
2015-09-03 06:31:19 -0500

    Speed up base_colorant_type on julia 0.3
    See https://github.com/timholy/Images.jl/issues/351

--- src/traits.jl
+++ src/traits.jl
@@ -144,6 +144,14 @@ base_color_type{C<:Colorant}(::Type{C}) = base_colorant_type(color_type(C))
 base_color_type(c::Colorant) = base_color_type(typeof(c))
 
 if VERSION < v"0.4.0-dev"
+    for CT in union(setdiff(parametric3, [RGB1,RGB4]), [Gray])
+        @eval base_colorant_type{C<:$CT}(::Type{C}) = $CT
+        @eval base_colorant_type{C<:AlphaColor{$CT}}(::Type{C}) = $(alphacolor(CT))
+        @eval base_colorant_type{C<:ColorAlpha{$CT}}(::Type{C}) = $(coloralpha(CT))
+    end
+    base_colorant_type{C<:RGB1}(::Type{C}) = RGB1
+    base_colorant_type{C<:RGB4}(::Type{C}) = RGB4
+    # Fallback
     base_colorant_type{C<:Colorant}(::Type{C}) = eval(C.name.name)  # slow, but oh well
 else
     @eval @generated function base_colorant_type{C<:Colorant}(::Type{C})
