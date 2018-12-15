9c82ab35e8a38cd0d61c4496b2a95b4166a4dd74
scls19fr <scls19fr@users.noreply.github.com>
2018-12-15 21:39:11 +0100

    Update types.jl

--- src/types.jl
+++ src/types.jl
@@ -160,7 +160,13 @@ struct RGB4{T<:Fractional} <: AbstractRGB{T}
 end
 RGB4(r::T, g::T, b::T) where {T<:Fractional} = RGB4{T}(r, g, b)
 
-"`RGB16` is Red-Green-Blue colorspace represented by two bytes (16 bits)."
+"""`RGB16` is Red-Green-Blue colorspace represented by two bytes (16 bits).
+
+1        |1          |        0
+5 4 3 2 1|0 9 8 7 6 5|4 3 2 1 0 
+---------|-----------|---------
+R R R R R|G G G G G G|B B B B B
+"""
 struct RGB16
     value::UInt16
 end
