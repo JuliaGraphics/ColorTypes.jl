3ce2edf666290fdc514056ee41ddb9202f9cd466
scls19fr <scls19fr@users.noreply.github.com>
2018-12-15 21:26:48 +0100

    Update ColorTypes.jl

--- src/ColorTypes.jl
+++ src/ColorTypes.jl
@@ -25,6 +25,7 @@ export YIQ, YCbCr
 export Gray
 
 export RGB24, ARGB32, Gray24, AGray32
+export RGB16
 
 # Note: the parametric TransparentColorColors are exported
 # algorithmically, see `@make_alpha` in types.jl.
