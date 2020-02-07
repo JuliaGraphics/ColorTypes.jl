b34520daadc046e4c818d99d67d9cf167d015d42
kimikage <kimikage.ceo@gmail.com>
2020-02-08 03:27:41 +0900

    Move tests for mapc/reducec/mapreducec to "test/operations.jl"

--- test/runtests.jl
+++ test/runtests.jl
@@ -96,60 +96,9 @@ c = convert(RGB24, ac)
 Set([DIN99d, DIN99o, DIN99, HSI, HSL, HSV, LCHab, LCHuv,
      LMS, Lab, Luv, XYZ, YCbCr, YIQ, xyY, BGR, RGB, Gray])
 
-## operations
 
 @test eltype(broadcast(RGB, [BGR(1,0,0)])) == RGB{N0f8}
 addred(x1::AbstractRGB, x2::AbstractRGB) = red(x1) + red(x2)
 @test addred.([RGB(1,0,0)], RGB(1.0,0,0)) == [2]
-
-@test @inferred(mapc(sqrt, Gray{N0f8}(0.04))) == Gray(sqrt(N0f8(0.04)))
-@test @inferred(mapc(sqrt, AGray{N0f8}(0.04, 0.4))) == AGray(sqrt(N0f8(0.04)), sqrt(N0f8(0.4)))
-@test @inferred(mapc(sqrt, GrayA{N0f8}(0.04, 0.4))) == GrayA(sqrt(N0f8(0.04)), sqrt(N0f8(0.4)))
-@test @inferred(mapc(x->2x, RGB{N0f8}(0.04,0.2,0.3))) == RGB(map(x->2*N0f8(x), (0.04,0.2,0.3))...)
-@test @inferred(mapc(sqrt, RGBA{N0f8}(0.04,0.2,0.3,0.7))) == RGBA(map(x->sqrt(N0f8(x)), (0.04,0.2,0.3,0.7))...)
-@test @inferred(mapc(x->1.5f0x, RGBA{N0f8}(0.04,0.2,0.3,0.4))) == RGBA(map(x->1.5f0*N0f8(x), (0.04,0.2,0.3,0.4))...)
-
-@test @inferred(mapc(max, Gray{N0f8}(0.2), Gray{N0f8}(0.3))) == Gray{N0f8}(0.3)
-@test @inferred(mapc(-, AGray{Float32}(0.3), AGray{Float32}(0.2))) == AGray{Float32}(0.3f0-0.2f0,0.0)
-@test @inferred(mapc(min, RGB{N0f8}(0.2,0.8,0.7), RGB{N0f8}(0.5,0.2,0.99))) == RGB{N0f8}(0.2,0.2,0.7)
-@test @inferred(mapc(+, RGBA{N0f8}(0.2,0.8,0.7,0.3), RGBA{Float32}(0.5,0.2,0.99,0.5))) == RGBA(0.5f0+N0f8(0.2),0.2f0+N0f8(0.8),0.99f0+N0f8(0.7),0.5f0+N0f8(0.3))
-@test @inferred(mapc(+, HSVA(0.1,0.8,0.3,0.5), HSVA(0.5,0.5,0.5,0.3))) == HSVA(0.1+0.5,0.8+0.5,0.3+0.5,0.5+0.3)
-@test_throws ArgumentError mapc(min, RGB{N0f8}(0.2,0.8,0.7), BGR{N0f8}(0.5,0.2,0.99))
-@test @inferred(mapc(abs, -2)) == 2
-
-@test @inferred(reducec(+, 0.0, Gray(0.3))) === 0.3
-@test @inferred(reducec(+, 1.0, Gray(0.3))) === 1.3
-@test @inferred(reducec(+, 0, Gray(0.3))) === 0.3
-@test @inferred(reducec(+, 0.0, AGray(0.3, 0.8))) === 0.3 + 0.8
-@test @inferred(reducec(+, 0.0, RGB(0.3, 0.8, 0.5))) === (0.3 + 0.8) + 0.5
-@test @inferred(reducec(+, 0.0, RGBA(0.3, 0.8, 0.5, 0.7))) === ((0.3 + 0.8) + 0.5) + 0.7
-@test @inferred(reducec(&, true, Gray(true)))
-@test !(@inferred(reducec(&, false, Gray(true))))
-@test !(@inferred(reducec(&, true, Gray(false))))
-
-@test @inferred(reducec(+, 0.0, 0.3)) === 0.3
-@test @inferred(reducec(+, 0, 0.3)) === 0.3
-@test @inferred(reducec(&, true, true))
-@test !(@inferred(reducec(&, false, true)))
-@test !(@inferred(reducec(&, true, false)))
-
-@test @inferred(mapreducec(x->x^2, +, 0.0, Gray(0.3))) === 0.3^2
-@test @inferred(mapreducec(x->x^2, +, 1.0, Gray(0.3))) === 1 + 0.3^2
-@test @inferred(mapreducec(x->x^2, +, 0, Gray(0.3))) === 0.3^2
-@test @inferred(mapreducec(x->x^2, +, 0.0, AGray(0.3, 0.8))) === 0.3^2 + 0.8^2
-@test @inferred(mapreducec(x->x^2, +, 0.0, RGB(0.3, 0.8, 0.5))) === (0.3^2 + 0.8^2) + 0.5^2
-@test @inferred(mapreducec(x->x^2, +, 0.0, RGBA(0.3, 0.8, 0.5, 0.7))) === ((0.3^2 + 0.8^2) + 0.5^2) + 0.7^2
-@test !(@inferred(mapreducec(x->!x, &, true, Gray(true))))
-@test !(@inferred(mapreducec(x->!x, &, false, Gray(true))))
-@test @inferred(mapreducec(x->!x, &, true, Gray(false)))
-@test !@inferred(mapreducec(x->!x, &, false, Gray(false)))
-
-@test @inferred(mapreducec(x->x^2, +, 0.0, 0.3)) === 0.3^2
-@test @inferred(mapreducec(x->x^2, +, 1.0, 0.3)) === 1 + 0.3^2
-@test @inferred(mapreducec(x->x^2, +, 0, 0.3)) === 0.3^2
-@test !(@inferred(mapreducec(x->!x, &, true, true)))
-@test !(@inferred(mapreducec(x->!x, &, false, true)))
-@test @inferred(mapreducec(x->!x, &, true, false))
-@test !@inferred(mapreducec(x->!x, &, false, false))
 
 end # ColorTypes
