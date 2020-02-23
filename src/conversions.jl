7a780b22bea2857a15bd4e6490ab607f291a0f47
kimikage <kimikage.ceo@gmail.com>
2020-02-23 21:59:01 +0900

    Improve promotion rules (Fixes #129) (#170)

--- src/conversions.jl
+++ src/conversions.jl
@@ -1,12 +1,71 @@
-Base.promote_rule(::Type{T1}, ::Type{T2}) where {T1<:AbstractGray,T2<:AbstractGray} = Gray{promote_type(eltype(T1), eltype(T2))}
-Base.promote_rule(::Type{T1}, ::Type{T2}) where {T1<:AbstractRGB,T2<:AbstractRGB} = RGB{promote_type(eltype(T1), eltype(T2))}
+# Get the promoted element type
+# Note that `promote_eltype` defined in "types.jl" means promote "arguments".
+function _promote_et(::Type{C1}, ::Type{C2}) where {C1<:Colorant, C2<:Colorant}
+    T1 = isconcretetype(eltype(C1)) ? eltype(C1) : eltypes_supported(C1)
+    T2 = isconcretetype(eltype(C2)) ? eltype(C2) : eltypes_supported(C2)
+    promote_type(T1, T2)
+end
+# Get the promoted base "color" type
+_promote_color(::Type{C},  ::Type{C})  where {C<:Color} = C
+_promote_color(::Type{C},  ::Type{C})  where {N, C<:ColorN{N}} = C
+_promote_color(::Type{C1}, ::Type{C2}) where {C1<:Color, C2<:Color} = Color
+_promote_color(::Type{C1}, ::Type{C2}) where {N1, C1<:ColorN{N1}, N2, C2<:ColorN{N2}} = ColorN{max(N1, N2)}
+_promote_color(::Type{C1}, ::Type{C2}) where {C1<:AbstractGray, C2<:Color} = _promote_color(C2, C2) # C2 may be AbstractRGB
+_promote_color(::Type{C1}, ::Type{C2}) where {C1<:Color, C2<:AbstractGray} = _promote_color(C1, C1) # C1 may be AbstractRGB
+_promote_color(::Type{C1}, ::Type{C2}) where {C1<:AbstractGray, N2, C2<:ColorN{N2}} = _promote_color(C2, C2)
+_promote_color(::Type{C1}, ::Type{C2}) where {N1, C1<:ColorN{N1}, C2<:AbstractGray} = _promote_color(C1, C1)
+_promote_color(::Type{C1}, ::Type{C2}) where {C1<:AbstractGray, C2<:AbstractGray} = Gray
+function _promote_color(::Type{C1}, ::Type{C2}) where {C1<:AbstractRGB, C2<:AbstractRGB}
+    (C1 === XRGB || C2 === XRGB) && return XRGB
+    (C1 === RGBX || C2 === RGBX) && return RGBX
+    RGB
+end
 
-Base.promote_rule(::Type{C3}, ::Type{Cgray}) where {C3<:Color3,Cgray<:AbstractGray} = base_colorant_type(C3){promote_type(eltype(C3), eltype(Cgray))}
-Base.promote_rule(::Type{C3}, ::Type{Cagray}) where {C3<:Color3,Cagray<:AbstractAGray} = alphacolor(base_colorant_type(C3)){promote_type(eltype(C3), eltype(Cagray))}
-Base.promote_rule(::Type{C3}, ::Type{Cgraya}) where {C3<:Color3,Cgraya<:AbstractGrayA} = coloralpha(base_colorant_type(C3)){promote_type(eltype(C3), eltype(Cgraya))}
+# Get the promoted transparent type
+_promote_alpha(::Type{C1}, ::Type{C2}) where {C1<:Colorant, C2<:Colorant} = AlphaColor
+_promote_alpha(::Type{C1}, ::Type{C2}) where {C1<:ColorAlpha, C2<:Color} = ColorAlpha
+_promote_alpha(::Type{C1}, ::Type{C2}) where {C1<:ColorAlpha, C2<:ColorAlpha} = ColorAlpha
+_promote_alpha(::Type{C1}, ::Type{C2}) where {C1<:ColorAlpha, C2<:AbstractAGray} = ColorAlpha
+_promote_alpha(::Type{C1}, ::Type{C2}) where {C1<:AbstractAGray, C2<:ColorAlpha} = ColorAlpha
+_promote_alpha(::Type{C1}, ::Type{C2}) where {C1<:Color, C2<:Color} = Color
 
-Base.promote_rule(::Type{C3}, ::Type{Cgray}) where {C3<:Transparent3,Cgray<:AbstractGray} = base_colorant_type(C3){promote_type(eltype(C3), eltype(Cgray))}
-Base.promote_rule(::Type{C3}, ::Type{Cgray}) where {C3<:Transparent3,Cgray<:TransparentGray} = base_colorant_type(C3){promote_type(eltype(C3), eltype(Cgray))}
+Base.promote_rule(::Type{C1}, ::Type{C2}) where {C1<:Colorant, C2<:Colorant} = _promote_rule(C1, C2)
+Base.promote_rule(::Type{C1}, ::Type{C2}) where {C1<:Color, C2<:TransparentColor} = Base.Bottom # to reduce rules
+
+Base.promote_rule(::Type{RGB24},   ::Type{Gray24})  = RGB24 # C vs. C
+Base.promote_rule(::Type{Gray24},  ::Type{RGB24})   = RGB24 # C vs. C
+Base.promote_rule(::Type{ARGB32},  ::Type{AGray32}) = ARGB32 # TC vs. TC
+Base.promote_rule(::Type{AGray32}, ::Type{ARGB32})  = ARGB32 # TC vs. TC
+Base.promote_rule(::Type{AGray32}, ::Type{Gray24})  = AGray32
+Base.promote_rule(::Type{AGray32}, ::Type{RGB24})   = ARGB32
+Base.promote_rule(::Type{ARGB32},  ::Type{Gray24})  = ARGB32
+Base.promote_rule(::Type{ARGB32},  ::Type{RGB24})   = ARGB32
+
+
+function _promote_rule(::Type{C1}, ::Type{C2}) where {C1<:Colorant, C2<:Colorant}
+    et, alpha = _promote_et(C1, C2), _promote_alpha(C1, C2)
+    Cp1, Cp2 = parametric_colorant(C1), parametric_colorant(C2)
+    color = _promote_color(base_color_type(Cp1), base_color_type(Cp2))
+
+    _with_et(C::UnionAll, et) = isconcretetype(et) ? C{et} : C
+    if !isabstracttype(color)
+        alpha <: Color && return _with_et(color, et)
+        c = color <: XRGB || color <: RGBX ? RGB : color
+        return _with_et(alpha <: ColorAlpha ? coloralpha(c) : alphacolor(c), et)
+    end
+
+    function _with_et(A::UnionAll, ::Type{Cb}, et) where {Nb, Cb<:ColorN{Nb}}
+        A <: Color && return isconcretetype(et) ? Color{et,Nb} : ColorN{Nb}
+        N = min(4, Nb + 1) # FIXME: N should be calculated based on C1 and C2
+        isconcretetype(et) ? A{C,et,N} where {C<:Cb{et}} : A{C,T,N} where {T, C<:Cb{T}}
+    end
+    function _with_et(A::UnionAll, ::Type{Cb}, et) where {Cb<:Color}
+        A <: Color && return isconcretetype(et) ? Color{et} : Color
+        isconcretetype(et) ? A{C,et} where {C<:Cb{et}} : A{C,T} where {T, C<:Cb{T}}
+    end
+    _with_et(alpha, color, et)
+end
+
 
 # no-op and element-type conversions, plus conversion to and from transparency
 # Colorimetry conversions are in Colors.jl
