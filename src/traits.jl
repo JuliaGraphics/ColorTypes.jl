6d925644249bad2ac599843f45166dc5f48aec0c
Johnny Chen <johnnychen94@hotmail.com>
2022-05-15 14:44:41 +0800

    Revert "Simplify display of colors and arrays of colors (#206)"
    This reverts commit 213e0deb56566c67a269af2c0a414b60da8844dd.

--- src/traits.jl
+++ src/traits.jl
@@ -461,10 +461,10 @@ Return a color instance of the specified type in which all components are NaN.
 # Examples
 ```jldoctest; setup = :(using ColorTypes)
 julia> ColorTypes.nan(RGB{Float32})
-RGB{Float32}(NaN, NaN, NaN)
+RGB{Float32}(NaN32,NaN32,NaN32)
 
 julia> ColorTypes.nan(AHSV{Float64})
-AHSV{Float64}(NaN, NaN, NaN, NaN)
+AHSV{Float64}(NaN,NaN,NaN,NaN)
 ```
 """
 nan(::Type{T}) where {T<:AbstractFloat} = convert(T, NaN)
