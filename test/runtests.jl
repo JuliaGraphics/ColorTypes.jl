aa68aa0db19a254d34c996d519084ed8d0543b75
kimikage <kimikage.ceo@gmail.com>
2026-09-24 12:01:23 +0900

    Separate code coverage and inference testing in CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,24 @@ using ColorTypes.FixedPointNumbers
 using Test
 using Aqua
 
+# Re-run inference-related tests in CI with the "inference" test argument
+if "inference" in ARGS
+    @testset "inference" begin
+        include("types.jl")
+        include("traits.jl")
+        include("operations.jl")
+    end
+    exit(0)
+end
+
+# The generation of coverage data might affect the type inference.
+# For this reason, when code coverage is enabled, `@inferred` is forced to pass through.
+if Base.JLOptions().code_coverage != 0
+    macro inferred(ex) # pass-through
+        return esc(ex)
+    end
+end
+
 @testset "Aqua tests" begin
     Aqua.test_all(ColorTypes)
 end
